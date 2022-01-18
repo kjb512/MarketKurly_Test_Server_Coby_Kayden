fafec167b67f1a8ff6a0fc5bb9aee65e1b040182
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-01-18 14:37:55 +0900

    feat : add question api crud

--- api-server-spring-boot-final 2-2/src/main/java/com/example/demo/src/question/QuestionDao.java
+++ api-server-spring-boot-final 2-2/src/main/java/com/example/demo/src/question/QuestionDao.java
@@ -1,10 +1,13 @@
 package com.example.demo.src.question;
 
+import com.example.demo.src.question.model.AnswerRes;
+import com.example.demo.src.question.model.QuestionRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class QuestionDao {
@@ -16,5 +19,49 @@ public class QuestionDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    public void createQuestion(int productIdx, int userIdx, String title, String question,String isLock){
+        String query = "insert into Question(title,question,userIdx,isLock,productIdx) values(?,?,?,?,?);";
+
+        this.jdbcTemplate.update(query,title,question,userIdx,isLock,productIdx);
+    }
+
+    public List<QuestionRes> getQuestionsWithAnswer(int productIdx){
+        String query = "select *\n" +
+                "from Question left join Answer A on Question.questionIdx = A.questionIdx and A.status='ACTIVE'\n" +
+                "where productIdx=? and Question.status='ACTIVE'\n" +
+                "order by Question.createAt DESC ;";
+
+        return this.jdbcTemplate.query(query,(rs, rowNum) -> new QuestionRes(rs.getInt("Question.questionIdx"),
+                rs.getInt("userIdx"),
+                rs.getString("title"),
+                rs.getString("question"),
+                rs.getString("isAnswer"),
+                rs.getString("isLock"),
+                rs.getTimestamp("Question.createAt"),
+                "",
+                new AnswerRes(rs.getInt("answerIdx"),
+                        rs.getString("answer"))),productIdx);
+    }
+
+    public void updateQuestion(int questionIdx, String title, String question, String isLock){
+        String query = "update Question set title=?,question=?,isLock=? where questionIdx=?;";
+        this.jdbcTemplate.update(query,title,question,isLock,questionIdx);
+    }
+
+    public void deleteQuestion(int questionIdx){
+        String deleteAnswerQuery = "update Answer set status='INACTIVE' where questionIdx=?;";
+        this.jdbcTemplate.update(deleteAnswerQuery,questionIdx);
+        String deleteQuestionQuery ="update Question set status='INACTIVE' where questionIdx=?;";
+        this.jdbcTemplate.update(deleteQuestionQuery,questionIdx);
+    }
+
+    public int creatAnswer(int answerIdx, String answer){
+        String query ="insert into Answer(questionIdx, answer) values (?,?);";
+        this.jdbcTemplate.update(query,answerIdx,answer);
+        String queryForResult = "select questionIdx\n" +
+                "from Answer\n" +
+                "where answerIdx=?";
+        return this.jdbcTemplate.queryForObject(queryForResult,int.class,answerIdx);
+    }
 
 }
--- api-server-spring-boot-final 2-2/src/main/java/com/example/demo/src/question/QuestionService.java
+++ api-server-spring-boot-final 2-2/src/main/java/com/example/demo/src/question/QuestionService.java
@@ -1,13 +1,20 @@
 package com.example.demo.src.question;
 
 
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponseStatus;
 import com.example.demo.src.product.ProductDao;
 import com.example.demo.src.product.ProductProvider;
+import com.example.demo.src.question.model.QuestionRes;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.example.demo.config.BaseResponseStatus.DATABASE_ERROR;
 
 // Service Create, Update, Delete 의 로직 처리
 @Service
@@ -26,4 +33,43 @@ public class QuestionService {
         this.jwtService = jwtService;
 
     }
+
+    public List<QuestionRes> createQuestion(int productIdx, int userIdx, String title, String question, String isLock) throws BaseException {
+        try{
+            questionDao.createQuestion(productIdx,userIdx,title,question,isLock);
+            return questionProvider.getQuestionsWithAnswer(productIdx);
+        }
+        catch (Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    public List<QuestionRes> updateQuestion(int productIdx,int questionIdx, String title, String question, String isLock) throws BaseException{
+        try{
+            questionDao.updateQuestion(questionIdx,title,question,isLock);
+            return questionProvider.getQuestionsWithAnswer(productIdx);
+        }catch (Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    public List<QuestionRes> deleteQuestion(int productIdx, int questionIdx) throws BaseException {
+        try {
+            questionDao.deleteQuestion(questionIdx);
+            return questionProvider.getQuestionsWithAnswer(productIdx);
+        } catch (Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    public List<QuestionRes> createAnswer(int productidx, int questionIdx, String answer) throws BaseException{
+        try{
+            questionDao.creatAnswer(questionIdx,answer);
+            return questionDao.getQuestionsWithAnswer(productidx);
+
+        }catch (Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
 }
